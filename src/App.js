75cb5c3a2fe84c09dc22401cb93af5b2058a3271
AbdulRehman623 <manirock623@outlook.com>
2024-06-23 18:52:23 +0400

    New File

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "react-bootstrap";
+import NewTests from "./NewTests";
 
 function App() {
   return (
     <div className="App">
+      <NewTests />
       <h1>E-comm Project </h1>
       <button>Nomal Button</button>
       <Button>Bootstrap Button</Button>
